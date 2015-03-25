e62270bfa9fcd728979a55575d22f19def6ae58c
Alexandre Theve <theve.alexandre@gmail.com>
2015-03-25 20:02:35 +0100

    Ajout de la cmd shell pour fixer le baudrate a 9600

--- client.js
+++ client.js
@@ -1,3 +1,8 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec("stty -F /dev/ttyO3 9600", puts);
+
 console.log('start waiting for serialport...');
 var serialport = require('node-serialport')
 var arDrone = require('ar-drone');
